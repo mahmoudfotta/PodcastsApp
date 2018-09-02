0390c063279ef175426210a518413666d6483e19
mahmoud gamal <mahmoudfotta@gmail.com>
2018-09-02 15:25:25 +0200

    first commit setup main tab bar controller

---
+++ PodcastsApp/MainTabBarController.swift
@@ -0,0 +1,41 @@
+//
+//  MainTabBarController.swift
+//  PodcastsApp
+//
+//  Created by mahmoud gamal on 9/2/18.
+//  Copyright © 2018 mahmoud gamal. All rights reserved.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        UINavigationBar.appearance().prefersLargeTitles = true
+        tabBar.tintColor = .purple
+        
+        setupViewControllers()
+    }
+    
+    //MARK:- setup functions
+    
+    fileprivate func setupViewControllers() {
+        viewControllers = [
+            generateNavigationController(with: ViewController(), title: "Favorites", image: #imageLiteral(resourceName: "favorites")),
+            generateNavigationController(with: ViewController(), title: "Search", image: #imageLiteral(resourceName: "search")),
+            generateNavigationController(with: ViewController(), title: "Downloads", image: #imageLiteral(resourceName: "downloads"))
+        ]
+    }
+    
+    //MARK:- helper functions
+    
+    fileprivate func generateNavigationController(with rootViewController: UIViewController, title: String, image: UIImage) -> UIViewController {
+        let navController = UINavigationController(rootViewController: rootViewController)
+        rootViewController.navigationItem.title = title
+        navController.tabBarItem.title = title
+        navController.tabBarItem.image = image
+        return navController
+    }
+}
--- PodcastsApp/ViewController.swift
+++ PodcastsApp/ViewController.swift
@@ -12,14 +12,9 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        view.backgroundColor = .white
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
