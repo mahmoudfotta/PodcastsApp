5afb6f377d0afdcd9b5827ea00c3d50e0c13e277
mahmoud gamal <mahmoudfotta@gmail.com>
2018-09-02 14:00:34 +0200

    Initial Commit

---
+++ PodcastsApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PodcastsApp
+//
+//  Created by mahmoud gamal on 9/2/18.
+//  Copyright © 2018 mahmoud gamal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
