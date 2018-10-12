1678689768174c318fc2883ac484fb78bffb082e
mahmoud gamal <mahmoudfotta@gmail.com>
2018-10-12 15:37:40 +0200

    delete downloaded episode from download tab

--- PodcastsApp/Controllers/DownloadsController.swift
+++ PodcastsApp/Controllers/DownloadsController.swift
@@ -9,41 +9,48 @@
 import UIKit
 
 class DownloadsController: UITableViewController {
-    
-    fileprivate let cellId = "cellId"
-    var episodes = UserDefaults.standard.downlodedEpisodes()
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        setupTableView()
-    }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        episodes = UserDefaults.standard.downlodedEpisodes()
-        tableView.reloadData()
-    }
-    
-    //MARK:- setup
-    
-    fileprivate func setupTableView() {
-        let nib = UINib(nibName: "EpisodeCell", bundle: nil)
-        tableView.register(nib, forCellReuseIdentifier: cellId)
-    }
-    
-    //MARK:= UITableview
-    
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return episodes.count
-    }
-    
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath) as! EpisodeCell
-        cell.episode = episodes[indexPath.row]
-        return cell
-    }
-    
-    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 134
-    }
+  
+  fileprivate let cellId = "cellId"
+  var episodes = UserDefaults.standard.downloadedEpisodes()
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    setupTableView()
+  }
+  
+  override func viewWillAppear(_ animated: Bool) {
+    super.viewWillAppear(animated)
+    episodes = UserDefaults.standard.downloadedEpisodes()
+    tableView.reloadData()
+  }
+  
+  //MARK:- setup
+  
+  fileprivate func setupTableView() {
+    let nib = UINib(nibName: "EpisodeCell", bundle: nil)
+    tableView.register(nib, forCellReuseIdentifier: cellId)
+  }
+  
+  //MARK:= UITableview
+  
+  override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+    let episode = self.episodes[indexPath.row]
+    episodes.remove(at: indexPath.row)
+    tableView.deleteRows(at: [indexPath], with: .automatic)
+    UserDefaults.standard.deleteEpisode(episode: episode)
+  }
+  
+  override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return episodes.count
+  }
+  
+  override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath) as! EpisodeCell
+    cell.episode = episodes[indexPath.row]
+    return cell
+  }
+  
+  override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+    return 134
+  }
 }
--- PodcastsApp/Extensions/UserDefaults.swift
+++ PodcastsApp/Extensions/UserDefaults.swift
@@ -9,45 +9,62 @@
 import Foundation
 
 extension UserDefaults {
-    
-    static let favoritedPodcastKey = "favoritedPodcastKey"
-    static let downloadedEpidodeKey = "downloadedEpidodeKey"
-    
-    func downloadEpisode(episode: Episode) {
-        do {
-            var downloadedEpisodes = downlodedEpisodes()
-            downloadedEpisodes.append(episode)
-            let data = try JSONEncoder().encode(downloadedEpisodes)
-            
-            UserDefaults.standard.set(data, forKey: UserDefaults.downloadedEpidodeKey)
-        } catch let encodeErr {
-            print("failed to encode episode with error", encodeErr)
-        }
+  
+  static let favoritedPodcastKey = "favoritedPodcastKey"
+  static let downloadedEpidodesKey = "downloadedEpidodeKey"
+  
+  func deleteEpisode(episode: Episode) {
+    let savedEpisodes = downloadedEpisodes()
+    let filteredEpisodes = savedEpisodes.filter { (e) -> Bool in
+      // you should use episode.collectionId to be safer with deletes
+      return e.title != episode.title
     }
     
-    func downlodedEpisodes() -> [Episode] {
-        guard let episodesData = data(forKey: UserDefaults.downloadedEpidodeKey) else {return []}
-        do {
-            let episodes = try JSONDecoder().decode([Episode].self, from: episodesData)
-            return episodes
-        } catch let decodeError {
-            print("error while decoding", decodeError)
-        }
-        return []
+    do {
+      let data = try JSONEncoder().encode(filteredEpisodes)
+      UserDefaults.standard.set(data, forKey: UserDefaults.downloadedEpidodesKey)
+    } catch let encodeErr {
+      print("Failed to encode episode:", encodeErr)
     }
-    
-    func savedPodcasts() -> [Podcast] {
-        guard let savedPodcastData = UserDefaults.standard.data(forKey: UserDefaults.favoritedPodcastKey) else {return []}
-        guard let savedPodcasts = NSKeyedUnarchiver.unarchiveObject(with: savedPodcastData) as? [Podcast] else {return []}
-        return savedPodcasts
+  }
+  
+  func downloadEpisode(episode: Episode) {
+    do {
+      var episodes = downloadedEpisodes()
+      //            episodes.append(episode)
+      //insert episode at the front of the list
+      episodes.insert(episode, at: 0)
+      let data = try JSONEncoder().encode(episodes)
+      UserDefaults.standard.set(data, forKey: UserDefaults.downloadedEpidodesKey)
+      
+    } catch let encodeErr {
+      print("Failed to encode episode:", encodeErr)
     }
-    
-    func deletePodcast(podcast: Podcast) {
-        let podcasts = savedPodcasts()
-        let filteredPodcasts = podcasts.filter { (p) -> Bool in
-            return p.trackName != podcast.trackName && p.artistName != podcast.artistName
-        }
-        let data = NSKeyedArchiver.archivedData(withRootObject: filteredPodcasts)
-        UserDefaults.standard.set(data, forKey: UserDefaults.favoritedPodcastKey)
+  }
+  
+  func downloadedEpisodes() -> [Episode] {
+    guard let episodesData = data(forKey: UserDefaults.downloadedEpidodesKey) else {return []}
+    do {
+      let episodes = try JSONDecoder().decode([Episode].self, from: episodesData)
+      return episodes
+    } catch let decodeError {
+      print("error while decoding", decodeError)
     }
+    return []
+  }
+  
+  func savedPodcasts() -> [Podcast] {
+    guard let savedPodcastData = UserDefaults.standard.data(forKey: UserDefaults.favoritedPodcastKey) else {return []}
+    guard let savedPodcasts = NSKeyedUnarchiver.unarchiveObject(with: savedPodcastData) as? [Podcast] else {return []}
+    return savedPodcasts
+  }
+  
+  func deletePodcast(podcast: Podcast) {
+    let podcasts = savedPodcasts()
+    let filteredPodcasts = podcasts.filter { (p) -> Bool in
+      return p.trackName != podcast.trackName && p.artistName != podcast.artistName
+    }
+    let data = NSKeyedArchiver.archivedData(withRootObject: filteredPodcasts)
+    UserDefaults.standard.set(data, forKey: UserDefaults.favoritedPodcastKey)
+  }
 }
